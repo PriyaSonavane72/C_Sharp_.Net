3b824cb86f3644ea967573877164296af93bd5fa
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2026-02-14 22:19:26 +0530

    Add files via upload

---
+++ Windows_Application/Assignment_03/MDV_Admission_System/MDV_Admission_System/Frm_Login.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace MDV_Admission_System
+{
+    partial class Frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Login = new System.Windows.Forms.Label();
+            this.lbl_UserName = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.btn_Logout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Login
+            // 
+            this.lbl_Login.AutoSize = true;
+            this.lbl_Login.BackColor = System.Drawing.Color.LightPink;
+            this.lbl_Login.Font = new System.Drawing.Font("Mongolian Baiti", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Login.Location = new System.Drawing.Point(364, 9);
+            this.lbl_Login.Name = "lbl_Login";
+            this.lbl_Login.Size = new System.Drawing.Size(371, 71);
+            this.lbl_Login.TabIndex = 0;
+            this.lbl_Login.Text = " Login From";
+            // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.AutoSize = true;
+            this.lbl_UserName.BackColor = System.Drawing.Color.LightBlue;
+            this.lbl_UserName.Font = new System.Drawing.Font("Mongolian Baiti", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UserName.Location = new System.Drawing.Point(194, 192);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(200, 44);
+            this.lbl_UserName.TabIndex = 1;
+            this.lbl_UserName.Text = "UserName";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.BackColor = System.Drawing.Color.LightBlue;
+            this.lbl_Password.Font = new System.Drawing.Font("Mongolian Baiti", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(204, 389);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(180, 44);
+            this.lbl_Password.TabIndex = 2;
+            this.lbl_Password.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Mongolian Baiti", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(651, 196);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 40);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Mongolian Baiti", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(651, 397);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(335, 40);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn_Submit.Font = new System.Drawing.Font("Mongolian Baiti", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.ForeColor = System.Drawing.Color.Crimson;
+            this.btn_Submit.Location = new System.Drawing.Point(423, 589);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(189, 60);
+            this.btn_Submit.TabIndex = 5;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = false;
+            this.btn_Submit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Logout
+            // 
+            this.btn_Logout.BackColor = System.Drawing.Color.Cornsilk;
+            this.btn_Logout.Font = new System.Drawing.Font("Mongolian Baiti", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Logout.ForeColor = System.Drawing.Color.Crimson;
+            this.btn_Logout.Location = new System.Drawing.Point(1020, 9);
+            this.btn_Logout.Name = "btn_Logout";
+            this.btn_Logout.Size = new System.Drawing.Size(132, 43);
+            this.btn_Logout.TabIndex = 6;
+            this.btn_Logout.Text = "Logout";
+            this.btn_Logout.UseVisualStyleBackColor = false;
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 770);
+            this.Controls.Add(this.btn_Logout);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_UserName);
+            this.Controls.Add(this.lbl_Login);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_Login";
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Login;
+        private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Button btn_Logout;
+    }
+}
+
